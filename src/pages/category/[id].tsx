670e91e3f00917130c62068e530f58f2bd087cd0
engsekine <engsekine@gmail.com>
2022-08-27 09:37:06 +0900

    update

--- src/pages/category/[id].tsx
+++ src/pages/category/[id].tsx
@@ -56,19 +56,21 @@ export default function ArchiveId({blog, author}: BlogQuery) {
                   {dayjs(blog.publishedAt).format('YYYY/MM/DD')}
                 </p>
 
-                <Link href={`/category/${blog.category.id}`}>
-                  <a className={styles.blogArticleCategory}>
-                    <svg xmlns='http://www.w3.org/2000/svg' className='icon icon-tabler icon-tabler-checkup-list' width='24' height='24' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'>
-                      <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
-                      <path d='M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2'></path>
-                      <rect x='9' y='3' width='6' height='4' rx='2'></rect>
-                      <path d='M9 14h.01'></path>
-                      <path d='M9 17h.01'></path>
-                      <path d='M12 16l1 1l3 -3'></path>
-                    </svg>
-                    {blog.category.name}
-                  </a>
-                </Link>
+                <div className={styles.blogArticleCategory}>
+                  <Link href={`/category/${blog.category.id}`}>
+                    <a>
+                      <svg xmlns='http://www.w3.org/2000/svg' className='icon icon-tabler icon-tabler-checkup-list' width='24' height='24' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'>
+                        <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+                        <path d='M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2'></path>
+                        <rect x='9' y='3' width='6' height='4' rx='2'></rect>
+                        <path d='M9 14h.01'></path>
+                        <path d='M9 17h.01'></path>
+                        <path d='M12 16l1 1l3 -3'></path>
+                      </svg>
+                      {blog.category.name}
+                    </a>
+                  </Link>
+                </div>
 
                 <p className={styles.blogArticleDescription}>{blog.description}</p>
                 <Link href={`/author`}>
